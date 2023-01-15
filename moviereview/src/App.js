d2e7101b29d44e06643a0af9c93a87b9ac28dece
Ishwardeep Anand <ishwardeepanand@Ishwardeeps-MacBook-Pro.local>
2023-01-15 11:11:47 -0800

    file structre is set. Need to start designing

--- moviereview/src/App.js
+++ moviereview/src/App.js
@@ -1,1 +1,11 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
+import About from "../src/components/About";
+import Home from "../src/components/Home";
+import Movies from "../src/components/Movies";
+import SignupBtn from "./components/SigupBtn";
+
+function translateDate(date){
+  //code
+   }
+
+export default translateDate;
---
+++ moviereview/src/components/About.js
@@ -0,0 +1,8 @@
+import React from 'react';
+function About() {
+  return (
+    <h1>Movies For You!</h1>
+  );
+}
+
+export default About;
