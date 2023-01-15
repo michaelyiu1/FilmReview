fa60efd8f80e43270329f35f78035aaa7ea44978
Ishwardeep Anand <ishwardeepanand@Ishwardeeps-MacBook-Pro.local>
2023-01-15 09:58:22 -0800

    still working on theme

---
+++ filmreview/src/components/CustomBtn.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import { Button } from '@material-ui/core'
+function CustomBtn(){
+        return(
+            <Button>Movies For You!</Button>
+
+        )
+}
+
+export default CustomBtn
