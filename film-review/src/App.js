cd7545bda279c506ec1a1cc720990d2762bea654
Ishwardeep Anand <ishwardeepanand@Ishwardeeps-MacBook-Pro.local>
2023-01-16 18:17:51 -0800

    me and Khiem are working on layout for the website through react

--- film-review/src/App.js
+++ film-review/src/App.js
@@ -1,12 +1,18 @@
 import React from 'react';
 import './App.css';
+import About from "./components/About";
+
 
 
 function App() {
   return (
-    <div className="App">
+  <div className="App">
+    <header>
      <h1>Movies For You!</h1>
+    </header>
+    <p>Movies For You is the best film review website in the market!</p>
     </div>
+
   );
 }
 
--- film-review/src/components/About.js
+++ film-review/src/components/About.js
@@ -5,6 +5,7 @@ function About() {
      
     </section>
   );
+
 }
 
 export default About;
