440a9288367621a8f3d150de6d29c247cde81568
Ishwardeep Anand <ishwardeepanand@Ishwardeeps-MacBook-Pro.local>
2023-01-18 17:05:25 -0800

    updated dropdown menu

--- book-layout/src/components/Home.js
+++ book-layout/src/components/Home.js
@@ -1,9 +1,11 @@
 import React from 'react'
+import moviePoster from '../Images/movie-poster.jpg';
 
 export default function Home() {
   return (
     <div>
       <h1>Home</h1>
+      <img src={moviePoster} className="movie-poster"/>
     </div>
   )
 }
